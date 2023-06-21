209b82fd292a04806e55e13afa892b9fc3a79a42
Adonis Yu <71581444+anurexia@users.noreply.github.com>
2023-06-21 23:31:16 +0800

    Add files via upload

---
+++ Sir Robby Project/solvingApproach.js
@@ -0,0 +1,41 @@
+/*
+?understand the problem
+!make sure na gets mo yung problem bago ka magstart ng code.
+*Kaya bang sabihin yung problem sa sarili mong words?
+*Ano yung mga input at output na kakailanganin?
+*yung output ba ay determined mula sa input
+*paano mo dapat ilabel yung mga importanteng data na part ng problem
+
+?research about the problem
+!example sa tic-tac-toe, learn the mechanics, rules etc.
+*Start with simple examples
+*Progress to more complex examples
+*explore examples with empty inputs
+*explore examples with invalid inputs
+
+?break it into smaller pieces
+!wag diretso solve, planuhin mo sa logical na steps. 
+*example in tic tac toe, maybe plan to have board firs
+*then players, then function for player selection
+
+?pseudocode
+!para maforce ka na isipin yung icocode mo, icomment mo nalang yung mga steps
+
+?implement and simplify
+!done is better than perfect, kung di mo alam, isolve mo yung mas simpleng problem
+*Hanapin mo yung pinaka mahihirapan ka at hayaan mo lang muna
+*Gumawa ka ng mas simpleng solution
+*then ibalik mo yung part na nahihirapan ka
+
+?refine and optimize
+!Kapag gumagana na yung code, irefine at ioptimize. maybe yung gamit mong algorithm
+!ay may inefficient time complexity, o baka namn mabilis nga pero sa space naman nabawi.
+
+*Tama ba yung naging result?
+*May naiisip ka pa bang ibang result?
+*gets mo ba kapag tinitignan mo yung solution
+*Pwede mo bang magamit yung solution sa ibang question
+*Pwede pa bang maimprove yung performance ng solution
+*Meron pa bang ibang way para marefactor yung code, like conventions, readbility etc.
+*Paano sinolve ng ibang programmer yung problem
+*/
